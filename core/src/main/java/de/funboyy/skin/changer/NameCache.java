9225e7731205c2933c799997492c8ca62c122a4f
Funboyy <72627658+Funboyy@users.noreply.github.com>
2023-12-30 21:36:13 +0100

    fixed: potential problems with rate limit and some issues when loading certain names

--- core/src/main/java/de/funboyy/skin/changer/NameCache.java
+++ core/src/main/java/de/funboyy/skin/changer/NameCache.java
@@ -1,15 +1,20 @@
 package de.funboyy.skin.changer;
 
+import com.google.gson.JsonObject;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.function.Consumer;
 import net.labymod.api.Laby;
 import net.labymod.api.client.resources.player.PlayerTextureService;
 import net.labymod.api.client.resources.player.PlayerTextureType;
+import net.labymod.api.util.io.web.request.types.GsonRequest;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class NameCache {
+
+  private static final String PROFILE_URL = "https://api.mojang.com/users/profiles/minecraft/%s";
 
   private final List<Entry> entries = new ArrayList<>();
 
@@ -40,12 +45,36 @@ public class NameCache {
 
     if (entry != null) {
       this.entries.remove(entry);
-      entry.reloadTexture();
     }
   }
   
   public void reloadTextures() {
-    this.entries.forEach(Entry::reloadTexture);
+    Laby.labyAPI().playerTextureService().invalidateTextures();
+  }
+
+  // Using mojang api here because it has a better rate limit for my use case
+  private static void loadUniqueId(final String name, final Consumer<UUID> result) {
+    GsonRequest.of(JsonObject.class).url(PROFILE_URL, name).async().execute(response -> {
+      if (response.hasException() || response.isEmpty()) {
+        result.accept(null);
+        return;
+      }
+
+      final JsonObject object = response.get();
+
+      if (!object.has("id") || !object.get("id").isJsonPrimitive()) {
+        result.accept(null);
+        return;
+      }
+
+      try {
+        result.accept(UUID.fromString(object.get("id").getAsString()
+            .replaceFirst("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})",
+                "$1-$2-$3-$4-$5")));
+      } catch (final IllegalArgumentException ignored) {
+        result.accept(null);
+      }
+    });
   }
 
   public static class Entry {
@@ -56,8 +85,8 @@ public class NameCache {
     private Entry(@NotNull final String name) {
       this.name = name;
 
-      Laby.labyAPI().labyNetController().loadUniqueIdByName(this.name, response -> {
-        this.uniqueId = response.getNullable();
+      loadUniqueId(this.name, response -> {
+        this.uniqueId = response;
         this.reloadTexture();
       });
     }
